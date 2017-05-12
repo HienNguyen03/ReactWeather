fb906c24bc78610d49669211d44a4766452eb82e
HienNguyen03 <lily31392@gmail.com>
2017-05-12 20:05:06 +0300

    Add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c053e4412a666d5507d1355c5fca2d59&units=metric';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
 
 module.exports = {
   getTemp: function (location) {
@@ -8,7 +8,6 @@ module.exports = {
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestUrl).then(function (res) {
-      debugger;
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
       } else {
