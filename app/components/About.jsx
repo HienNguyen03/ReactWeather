7527344c49f845be92a754bb1d7d3b6edc811f8d
HienNguyen03 <lily31392@gmail.com>
2017-05-12 11:50:32 +0300

    Style part 3

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,19 +1,24 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About Component</h3>
-//     );
-//   }
-// });
-
 //stateless, haing only render function, does not maintain any state
 var About = (props) => {
   return (
     <div>
-        <h3>About Page</h3>
-        <p>Welcome to ReactWeather application</p>
+        <h1 className="text-center">About Page</h1>
+        <p>This is a weather application build on React.</p>
+        <p>
+          Here are some of the tools I used:
+        </p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the
+              JavaScript framework used.
+          </li>
+          <li>
+            <a href="http://openWeathermap.org">Open Weather Map</a> - Searching
+              for weather data by city name.
+          </li>
+        </ul>
     </div>
 
   );
